602eaa6ebf33281749e3964ab43336173e9763f6
Steve Archer <sarcher84@gmail.com>
2022-02-07 13:52:39 -0500

    init

---
+++ algo/balanceIndex.js
@@ -0,0 +1,23 @@
+// finished ~20 minutes - last 5 minutes spent debugging that I didnt increment i within the while loop (line 13 -> i++)
+
+const balanceIndex = (arr, sum=0, index=0) => {
+    if(index>=arr.length) return -1
+    for(let i=0;i<arr.length;i++){
+        sum += arr[i]
+    }
+    let total = 0
+    let i = 0
+    sum-=arr[index]
+    while(i<index){
+        total += arr[i]
+        i++
+    }
+    if(total == sum/2)return index
+    else {
+        index +=1
+        return balanceIndex(arr, sum = 0, index)
+    }
+}
+console.log(balanceIndex([1,2,4,9,20])) // -1
+console.log(balanceIndex([4,1,2,4,9,-20,18])) // 3
+console.log(balanceIndex([-2,5,7,0,3])) // 2
---
+++ fundamentals/StartingJava.java
@@ -0,0 +1,7 @@
+public class StartingJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Steve Archer");
+        System.out.println("I am 37 years old");
+        System.out.println("My hometown is Derry, NH");
+    }
+}
